bc08250abe47c758215a599e1537636cb287cb8f
Ikachenko <28ikaliber@gmail.com>
2025-05-15 22:24:33 +0700

    a

--- script.js
+++
@@ -1,8 +0,0 @@
-function adjustViewportHeight() {
-    const vh = window.innerHeight * 0.01; // Calculate 1% of the viewport height
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-// Adjust on load and resize
-window.addEventListener('load', adjustViewportHeight);
-window.addEventListener('resize', adjustViewportHeight);
