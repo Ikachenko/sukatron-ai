bc0a0fef5efe16b018ddf0eb5e9bfeaa6f370e83
Ikachenko <28ikaliber@gmail.com>
2025-05-16 11:20:54 +0700

    upload

--- function.js
+++ function.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const intro = document.getElementById("intro");
     const aiContainer = document.getElementById("AI");
+    const body = document.getElementById("body");
     
     document.addEventListener("click", function() {
         if (intro.style.display !== "none") {
             intro.style.display = "none";
             aiContainer.classList.add("iframe-visible");
+            document.querySelectorAll('body > *:not(#ai-container)').forEach(function(element) {
+                element.style.filter = 'blur(10px)';
+            });
         }
     });
 
